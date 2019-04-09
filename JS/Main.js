af4c73d5129a1736dd96a73a05294da322a96206
Asbjørn <fishermansfriend_93@hotmail.com>
2019-04-09 09:22:58 +0200

    Ting
     Ting

--- JS/Main.js
+++ JS/Main.js
@@ -1,3 +1,21 @@
-function test () {
-  alert("Hello World");
+
+function Introduksjon_show() {
+  alert("Intro");
 }
+
+function Oversikt_show() {
+  console.log("Nice")
+}
+
+function Detaljer_show() {
+
+}
+
+function Sammenlign_show() {
+
+}
+
+var Beskrivelser_url = "http://wildboy.uib.no/~tpe056/folk/";
+var Befolkning_url = "http://wildboy.uib.no/~tpe056/folk/104857.json";
+var Sysselsatte_url =  "http://wildboy.uib.no/~tpe056/folk/100145.json";
+var Utdanning_url = "http://wildboy.uib.no/~tpe056/folk/85432.json";
