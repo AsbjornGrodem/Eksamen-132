0cb6358bc0c00bf1bb528da7dfe572dad7181a77
Eirik <eirikenes@outlook.com>
2019-04-29 08:40:17 +0200

    dfvd

--- JS/Main.js
+++ JS/Main.js
@@ -2,6 +2,10 @@ var Beskrivelser_url = "http://wildboy.uib.no/~tpe056/folk/";
 var Befolkning_url = "http://wildboy.uib.no/~tpe056/folk/104857.json";
 var Sysselsatte_url =  "http://wildboy.uib.no/~tpe056/folk/100145.json";
 var Utdanning_url = "http://wildboy.uib.no/~tpe056/folk/85432.json";
+var x = document.getElementById("Introduksjon_div");
+var x = document.getElementById("Oversikt_div");
+var x = document.getElementById("Detaljer_div");
+var x = document.getElementById("Sammenligning_div");
 
               //Funksjon for å hente informasjon fra Wildboy
 function Fetch() {
@@ -27,6 +31,22 @@ $(document).ready(function(){
     $("Detaljer_div").toggle();
   });
 });
+
+function show(button) {
+
+  if button === 1 {
+    console.log("du trykket på knapp 1");
+  };
+
+};
+
+  /*if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+*/
 /*
 function Introduksjon_show() {
   var x = document.getElementById("Introduksjon_div");
@@ -64,5 +84,4 @@ function Sammenlign_show() {
   }
 
 }
-
 */
